8786ee9702ba015d52a1d376bb4fddf4279d5484
root <root@vmenda0.example.com>
2018-07-30 06:14:08 -0400

    initial workstation cookbook commit

---
+++ recipes/install.rb
@@ -0,0 +1,19 @@
+package 'tree' do 
+	action :install
+end
+package 'ntp' do
+
+	action :install
+end
+file '/training/motd' do
+	content "This file is the property of venugopal"
+	owner 'mapr'
+	group 'mapr'
+end
+service 'ntpd' do
+	action [:enable, :start]
+
+end
+package 'git' do
+	action :install
+end
